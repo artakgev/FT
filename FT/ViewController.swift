535d29d3ffe0fa05656910dec6825109bfcbdfaa
Artak Gevorgyan <artakreg@gmail.am>
2024-11-16 11:09:51 -0800

    Added empty project

---
+++ FT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FT
+//
+//  Created by Artak Gevorgyan on 16.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FTTests/FTTests.swift
@@ -0,0 +1,17 @@
+//
+//  FTTests.swift
+//  FTTests
+//
+//  Created by Artak Gevorgyan on 16.11.24.
+//
+
+import Testing
+@testable import FT
+
+struct FTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
